8c308350b049102e8cdfe6e31e28b6e80fd45b33
fuwamaki <variety.fuwa.3@gmail.com>
2022-01-03 13:49:08 +0900

    add tableView

--- Sample/View/MainViewController.swift
+++ Sample/View/MainViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }

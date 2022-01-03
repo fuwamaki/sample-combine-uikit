d720779195b6ff07d93d7d05cc6b3877e2932da4
fuwamaki <variety.fuwa.3@gmail.com>
2022-01-03 16:03:55 +0900

    add MockAPIClient

---
+++ SampleTests/MockAPIClient.swift
@@ -0,0 +1,43 @@
+//
+//  MockAPIClient.swift
+//  SampleTests
+//
+//  Created by fuwamaki on 2022/01/03.
+//
+
+import Foundation
+@testable import Sample
+
+class MockAPIClient: APIClientable {
+
+    let mockGithubRepoList: GithubRepoList = GithubRepoList(
+        items: [GithubRepo(
+            fullName: "testName",
+            stargazersCount: 10,
+            htmlUrl: "https://www.google.com",
+            owner: GithubRepoOwner(avatarUrl: "https://www.google.com")
+        )]
+    )
+
+    private let isSuccess: Bool
+
+    required init(isSuccess: Bool) {
+        self.isSuccess = isSuccess
+    }
+
+    func fetchImageData(url: URL) async throws -> Data {
+        if isSuccess {
+            return Data()
+        } else {
+            throw NSError(domain: "api error", code: 400, userInfo: nil)
+        }
+    }
+
+    func fetchGithubRepo(query: String) async throws -> GithubRepoList {
+        if isSuccess {
+            return mockGithubRepoList
+        } else {
+            throw NSError(domain: "api error", code: 400, userInfo: nil)
+        }
+    }
+}
